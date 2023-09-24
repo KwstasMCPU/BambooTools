e0bdcbc6bdfa8be17c46d6c203376c0c794dfbe2
kostas_marav <kostas.maravegias@marinetraffic.com>
2023-09-24 19:58:14 +0200

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
 from setuptools import setup, find_packages
-from pathlib import Path
 
-this_directory = Path(__file__).parent
-long_description = (this_directory / "README.md").read_text()
+with open("README.md", "r") as f:
+    long_description = f.read()
 
 setup(
     name='BambooTools',
-    version='0.2.0-alpha',
+    version='0.2.0',
     author='Konstantinos Maravegias',
     author_email='kwstas.maras@gmail.com',
     packages=find_packages(),
@@ -16,9 +15,11 @@ setup(
     long_description=long_description,
     long_description_content_type='text/markdown',
     install_requires=[
-        "pandas >= 1.5.3",
-        "pathlib",
+        "pandas >= 1.5.3"
         ],
+    extras_require={
+        "dev": ["pytest", "twine", "seaborn"]
+        },
     classifiers=["Development Status :: 3 - Alpha",
                  "Programming Language :: Python :: 3",
                  "License :: OSI Approved :: MIT License"
